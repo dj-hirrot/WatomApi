79fd7312e03fe69ce499be2708f90c905aa19c35
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-09 09:43:32 +0000

    create red test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = Category.new(name: "category")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "category naem should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "category naem should be unique" do
+    dup_category = @category.dup
+    dup_category.name = @category.name.uppercase
+    @category.save
+    assert_not dup_category.valid?
+  end
 end
