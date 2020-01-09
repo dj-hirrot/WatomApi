acfcb9789ab259e1e054814a6aa409a68d25e597
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-09 09:36:16 +0000

    name is unique with index

--- db/migrate/20200109093500_create_categories.rb
+++ db/migrate/20200109093500_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[6.0]
     create_table :categories do |t|
       t.string :name
 
+      t.index :name, unique: true
       t.timestamps
     end
   end
