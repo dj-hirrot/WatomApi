c8b8348f400dacd19c86db6620d0970ff7eb476c
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-10 11:37:13 +0000

    create skills test

--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class SkillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    _category = Category.create(name: 'category')
+    @skill = Skill.new(category_id: _category, name: 'skill')
+  end
+
+  test 'should be valid' do
+    assert @skill.valid?
+  end
+
+  test 'name should be present' do
+    @skill.name = ' '
+    assert_not @skill.valid?
+  end
 end
