d5677024be595c99aa61c3a593100ba5df7d6ad9
dj-hirrot <hirotoshibutani@gmail.com>
2020-01-10 11:54:46 +0000

    add uniqueness validation

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,6 @@
 class Skill < ApplicationRecord
+  before_save { self.name.downcase! }
   belongs_to :category
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
